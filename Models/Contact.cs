cc4c927dd576ef273798dece55efd9d692352a57
LydianLights <rane.fields@gmail.com>
2017-10-13 08:24:13 -0700

    Add contact class

---
+++ Models/Contact.cs
@@ -0,0 +1,36 @@
+using System;
+
+namespace AddressBook.Modeels
+{
+    public class Contact
+    {
+        private static List<Contact> _instances = new List<Contact>{};
+        private string _name;
+        private int _id;
+
+        public Contact(string name)
+        {
+            _name = name;
+            _id = _instances.Count;
+            _instances.Add(this);
+        }
+
+        public string GetName()
+        {
+            return _name;
+        }
+        public int GetId()
+        {
+            return _id;
+        }
+
+        public static Contact FindById(int id)
+        {
+            return _instances[id];
+        }
+        public static List<Contact> GetAllInstances()
+        {
+            return _instances;
+        }
+    }
+}
